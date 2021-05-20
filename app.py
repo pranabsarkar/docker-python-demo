cd30282474f030565f7df3eedf364b53f8120c4c
Pranab Sarkar <sarkarpranab66@gmail.com>
2021-05-20 14:21:49 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import json
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+
+@app.route("/myjson", methods=["POST", "GET"])
+def handle():
+    try:
+        RequestKey = request.get_json(force=True)
+        return json.dumps(RequestKey)
+    except Exception as e:
+        return json.dumps(e)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5540"), debug=True)
